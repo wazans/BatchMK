7ec1af35a8d24c0c95bd535a0e0acd63476f5651
Wasim Ansari <wasim.ansari@globalsign.com>
2022-02-14 15:58:38 +0530

    my first commit

---
+++ GuruProject/runme.bat
@@ -0,0 +1,9 @@
+set projectLocation=C:\Users\Wasim Ansari\eclipse-workspace\GuruProject
+
+cd %projectLocation%
+
+set classpath=%projectLocation%\bin;%projectLocation%\lib\*
+
+java org.testng.TestNG %projectLocation%\testng.xml
+
+pause
---
+++ GuruProject/src/main/java/com/corp/qa/Utilities/TestUtil.java
@@ -0,0 +1,21 @@
+package com.corp.qa.Utilities;
+
+import java.io.IOException;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import com.corp.qa.base.Base;
+
+
+public class TestUtil extends Base {
+
+	public static long PAGE_LOAD_TIMEOUT = 20;
+	public static long IMPLICIT_WAIT = 20;
+
+	public TestUtil() throws IOException {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+}
